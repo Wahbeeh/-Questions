907c9db3f246eedc91239dc6d55c1b1a5fb38593
Wahbeeh <122154429+Wahbeeh@users.noreply.github.com>
2023-02-23 10:19:53 +0100

    Add files via upload

---
+++ Questions.py
@@ -0,0 +1,11 @@
+older = int (input("Hur gammal är du? :"))
+if older >=18:
+    #print (" Du är välkomn")
+    pen = int (input ("Hur mycket pengar har du?"))
+    if pen >=500:
+        print ("Välkomen")
+    else:
+        print(" du får spara ")
+
+else:
+    print (" vänta ")
